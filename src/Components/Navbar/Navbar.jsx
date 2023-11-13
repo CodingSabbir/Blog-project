7810ef57ddd59389d3b38c65baf57709308a253b
MD.Sabbir Rahman <mdsabbirrahmanm740@gmail.com>
2023-11-13 21:02:50 +0600

    facebook

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,22 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './Navbar.css'
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../Firebase/FirebaseAuth';
+import { signOut } from 'firebase/auth';
 
 const Navbar = () => {
 const [isMobileMenuOpen, setMobileMenuOpen] =useState(false);
 const toggleMobileMenu = () => {
 setMobileMenuOpen(!isMobileMenuOpen);
 };
+
+const [user]=useAuthState(auth)
+console.log(user)
+const handleSignOut=()=>{
+    signOut(auth)
+}
+
 return (
 <div>
 
@@ -26,24 +36,24 @@ return (
 
 
               
-
-              <NavLink to={'/signUp'}>
-              <a href="#"
-                        className="   font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800">
-                        SignUp
-                    </a>
-              </NavLink>
-
-              <NavLink to={'/login'}>
-               <a href="#"
-                    className="  text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800">
-                     Login
-                </a>
-               </NavLink>
-
-
-
-
+                {
+                    user ? (
+                        <NavLink to={'/login'}>
+                        <a href="#"
+                            onClick={handleSignOut}
+                             className="  text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800">
+                              Login
+                         </a>
+                        </NavLink>
+                    ):(
+                        <NavLink to={'/signUp'}>
+                        <a href="#"
+                                  className="   font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800">
+                                  SignUp
+                              </a>
+                        </NavLink>
+                    )
+                }
 
                 <button data-collapse-toggle="mobile-menu-2" type="button" onClick={toggleMobileMenu}
                     className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -1,67 +1,255 @@
+
+// import React, { useState } from 'react';
+// import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+// import auth from '../Firebase/FirebaseAuth';
+
+
+
+// const SignUp = () => {
+//   const [email, setEmail] = useState('');
+//   const [password, setPassword] = useState('');
+//   const [message,setMessage] = useState('')
+//   const [users, setUser] = useState('')
+
+//   const [createUserWithEmailAndPassword,user,error,loading ] = useCreateUserWithEmailAndPassword(auth);
+// console.log(user)
+//   const messageDiv=document.querySelector('#messageDiv')
+
+//   if (error) {
+//       messageDiv.innerHTML=error
+//     }
+//     if (loading) {
+//       messageDiv.innerHTML='Loading...'
+//     }
+//     if (user) {
+//       messageDiv.innerHTML='Login successfull'
+//     }
+ 
+
+//   return (
+//     <div className="bg-gray-100 flex justify-center items-center h-screen mt-10">
+//       <div className="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
+//         <h1 className="text-2xl font-semibold mb-4">Sign Up</h1>
+//         <form >
+//           <div className="mb-4">
+//             <label htmlFor="username" className="block text-gray-600">
+//               UserName
+//             </label>
+//             <input
+//               value={users}
+//               onChange={(e) => setUser(e.target.value)}
+//               type="name"
+//               id="username"
+//               name="username"
+//               className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+//               autoComplete="off"
+//             />
+//           </div>
+
+//           <div className="mb-4">
+//             <label htmlFor="email" className="block text-gray-600">
+//               Email
+//             </label>
+//             <input
+//               value={email}
+//               onChange={(e) => setEmail(e.target.value)}
+//               type="email"
+//               id="email"
+//               name="email"
+//               placeholder="Enter your email"
+//               className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+//               autoComplete="off"
+//             />
+//           </div>
+
+//           <div className="mb-4">
+//             <label htmlFor="password" className="block text-gray-600">
+//               Password
+//             </label>
+//             <input
+//              value={password}
+//              onChange={(e) => setPassword(e.target.value)}
+//               type="password"
+//               id="password"
+//               name="password"
+//               className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+//               autoComplete="off"
+//             />
+//           </div>
+
+//           <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">
+//             Your message
+//           </label>
+//           <textarea
+//             value={message}
+//             onChange={(e) => setMessage(e.target.value)}
+//             id="message"
+//             rows="4"
+//             className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+//             placeholder="Your message..."
+//           ></textarea>
+          
+//           <div className="mt-6 text-blue-500">
+//             <a href="#" className="hover:underline">
+//               Login up Here
+//             </a>
+//           </div>
+
+//           <div id='messageDiv'></div>
+
+//           <div className="flex gap-5 mt-5">  
+//             <button  onClick={() => createUserWithEmailAndPassword(email, password,message,users)} type="submit" className="bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-md py-2 px-4 w-full">
+//               Sign Up
+//             </button>
+//             <button type="button"  className="bg-red-500 hover:bg-red-600 text-white font-semibold rounded-md py-2 px-4 w-full">
+//               Reset
+//             </button>
+//           </div>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default SignUp;
+
+
+
+
+
+
 import React, { useState } from 'react';
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import auth from '../Firebase/FirebaseAuth';
-
+import { NavLink } from 'react-router-dom';
 
 const SignUp = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [users, setUsers] = useState('');
+  const [message,setMessage] = useState('')
+  const [users, setUsers] = useState('')
+  const [createUserWithEmailAndPassword, user, error, loading] = useCreateUserWithEmailAndPassword(auth,{sendEmailVerification:true});
+  // console.log(user)
+  const handleSignUp = () => {
+    if (!email || !password || !users || !message) {
+      showMessage('Please enter both email and password  userName and  text.');
+      return;
+    }
 
-  const [
-    createUserWithEmailAndPassword,
-    user,
-    loading,
-    error,
-  ] = useCreateUserWithEmailAndPassword(auth);
-    return (
+    showMessage('Loading...');
 
- <div>       
-   
-<div class="bg-gray-100 flex justify-center items-center h-screen mt-10">
-    
-{/* <div class="w-1/2 h-screen hidden lg:block">
-  <img src="https://placehold.co/800x/667fff/ffffff.png?text=Your+Image&font=Montserrat" alt="Placeholder Image" class="object-cover w-full h-full"/>
-</div> */}
+    createUserWithEmailAndPassword(email, password,message,users)
+      .then((userCredential) => {
+        // Handle successful signup
+        const user = userCredential.user;
+        showMessage('Login successful');
+      })
+      .catch((error) => {
+        // Handle signup error
+        showMessage(error.message);
+      });
+  };
 
-<div class="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
-  <h1 class="text-2xl font-semibold mb-4">Sign Up</h1>
-  <form action="#" method="POST">
-
-  <div class="mb-4">
-      <label for="username" class="block text-gray-600">UserName</label>
-      <input value={users}  onChange={(e) => setUsers(e.target.value)} type="name" id="username" name="username" class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500" autocomplete="off"/>
-    </div>
-
-    <div class="mb-4">
-      <label for="email" class="block text-gray-600">Email</label>
-      <input   value={email}  onChange={(e) => setEmail(e.target.value)} type="email" id="email" name="email" class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500" autocomplete="off"/>
-    </div>
-
-    <div class="mb-4">
-      <label for="password" class="block text-gray-600">Password</label>
-      <input  value={password} onChange={(e) => setPassword(e.target.value)} type="password" id="password" name="password" class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500" autocomplete="off"/>
-    </div>
-
-    <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
-    <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your message..."></textarea>
+  const showMessage = (message) => {
+    const messageDiv = document.querySelector('#messageDiv');
+    if (messageDiv) {
+      messageDiv.innerHTML = message;
+    }
+  };
   
-    <div class="mt-6 text-blue-500 ">
-    <a href="#" class="hover:underline">Login up Here</a>
-  </div>
+  const handleReset = () => {
+    setEmail('');
+    setPassword('');
+    setMessage('');
+    setUsers('');
+  };
 
-    <div className='flex gap-5 mt-5'>
-    <button onClick={() => createUserWithEmailAndPassword(email, password,users)} type="submit" class="bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-md py-2 px-4 w-full">Sign Up</button>
-    <button type="submit" class="bg-red-500 hover:bg-red-600 text-white font-semibold rounded-md py-2 px-4 w-full">Reset</button>
+  return (
+    <div className="bg-gray-100 flex justify-center items-center mt-14 md:h-screen md:mt-5">
+      <div className="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
+        <h1 className="text-2xl font-semibold mb-4">Sign Up</h1>
+        <form>
+      
+          <div className="mb-4">
+            <label htmlFor="username" className="block text-gray-600">
+              UserName
+             </label>
+              <input
+               value={users}
+               onChange={(e) => setUsers(e.target.value)}
+              type="name"
+              id="username"
+              name="username"
+              className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+              autoComplete="off"
+            />
+          </div>
+
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-gray-600">
+              Email
+            </label>
+            <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Enter your email"
+              className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+              autoComplete="off"
+            />
+          </div>
+
+          <div className="mb-4">
+            <label htmlFor="password" className="block text-gray-600">
+              Password
+            </label>
+            <input
+             value={password}
+             onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              id="password"
+              name="password"
+              className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+              autoComplete="off"
+            />
+          </div>
+
+          <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">
+            Your message
+          </label>
+          <textarea
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            id="message"
+            rows="4"
+            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Your message..."
+          ></textarea>
+
+        <NavLink to={'/login'}>
+        <div className="mt-6 text-blue-500">
+           <a href="#" className="hover:underline">
+             Login Here
+          </a>
+         </div>
+        </NavLink>
+
+       
+          <div id='messageDiv'></div>
+          <div className="flex gap-5 mt-5">
+            <button onClick={handleSignUp} type="button" className="bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-md py-2 px-4 w-full">
+              Sign Up
+            </button>
+            <button onClick={handleReset} type="reset" className="bg-red-500 hover:bg-red-600 text-white font-semibold rounded-md py-2 px-4 w-full">
+              Reset
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-  </form>
-
-  
-</div>
-</div>
-
-        </div>
-    );
+  );
 };
 
 export default SignUp;
